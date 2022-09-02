67053eac3acb86579a444a2d8bbbf5de4a387ef1
techen0201 <techen0201@outlook.com>
2022-09-02 18:05:35 +0800

    e

--- 1/9.2.2.py
+++ 1/9.2.2.py
@@ -1,9 +1,34 @@
 #迭代器
+
 def inter1():
     list1=[1,2,3,4]
     it = iter(list1)
     print([x for x in it])
 
+def inter2():
+    import sys
+    list2=[1,2,3,4]
+    it=iter(list2)
+    while True:
+        try:
+            print(next(it),end=' ')
+        except StopIteration:
+            sys.exit()
 
+#把一个类作为迭代器
+class MyNumbers:
+    def __iter__(self):
+        self.a=1
+        return self
+    def __next__(self):
+        self.a
+        self.a+=1
+        return self.a
 if __name__=='__main__':
-    inter1()
+    #inter1()
+    #inter2()
+    myclass=MyNumbers()
+    myiter=iter(myclass)
+    print(next(myiter))
+    print(next(myiter))
+    print(next(myiter))

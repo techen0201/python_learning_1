f0b6c381f5c0ee19856cc690c9530edc06a03951
techen0201 <techen0201@outlook.com>
2022-09-03 07:08:52 +0800

    e

--- 1/9.3.1-生成器.py
+++ 1/9.3.1-生成器.py
@@ -4,18 +4,20 @@ import numpy as np
 def fibonacci(n):
     a,b,count=0,1,0
     #方法1
-    '''
+
     while True:
         if count>n:
             return
         yield a
         a,b=b,a+b
         count+=1
+
     '''
     #方法二
     for count in np.arange(n+1):
         yield a
         a,b=b,a+b
+    '''
 if __name__=='__main__':
     f=fibonacci(10)
     while True:

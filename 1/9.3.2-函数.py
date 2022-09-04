9de9dc1d2b2ea368f119a8a82ad1d6b6add54cc0
techen0201 <techen0201@outlook.com>
2022-09-04 10:04:01 +0800

    e

---
+++ 1/9.3.2-函数.py
@@ -0,0 +1,14 @@
+#函数
+#两个数比较
+def max(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+#计算面积
+def area(width,height):
+    return width*height
+
+if __name__=='__main__':
+    #print(max(4,5))
+    print(area(4,5))
